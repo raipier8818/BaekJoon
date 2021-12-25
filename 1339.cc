ad9e5e17fbe01164f4d1b0ebf1aca5d388c813a4
raipier8818 <raipier8818@hanyang.ac.kr>
2021-12-25 11:43:13 +0900

    solve 1339 c++

--- 1339.cc
+++ 1339.cc
@@ -1,26 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
-vector<string> a;
+int N;
 
-char arr[100];
-
-int find(string a, string b){
-    if(a.size() > b.size()){
-        
-    }
-}
-
-
+map<char, int> m;
+string str[10];
 
 int main(){
-    cin >> n;
-    for(int i = 0; i < n; i++){
-        string r;
-        cin >> r;
-        a.push_back(r);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        cin >> str[i];
+        int dec = 1;
+        for(int j = str[i].size() - 1; j >= 0; j--){
+            m[str[i][j]] += dec;
+            dec *= 10;
+        }
     }
 
+    vector<pair<char, int>> v(m.begin(), m.end());
+
+    sort(v.begin(), v.end(), [](pair<char, int> a, pair<char, int> b){
+        return a.second > b.second;
+    });
+
+    // for(int i = 0; i < v.size(); i++){
+    //     cout << v[i].first << " " << v[i].second << endl;
+    // }
+
+    int ans = 0;
+
+    for(int i = 9, j = 0; j < v.size(); i--, j++){
+        ans += v[j].second * i;
+    }
+
+    cout << ans << endl;
     
 }
