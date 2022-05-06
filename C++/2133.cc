dcc49b1bfb1d73bedbc5d9a9b3c0d8ee93b4881f
Lake <63098401+raipier8818@users.noreply.github.com>
2022-05-06 18:01:25 +0900

    Update 2133.cc

--- C++/2133.cc
+++ C++/2133.cc
@@ -1,19 +1,23 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+
 using namespace std;
 
 int n;
 long long dp[31];
 
+
 int main(){
     cin >> n;
 
     dp[2] = 3;
-    dp[4] = 9 + 2;
-
-    for(int i = 6; i <= n; i+=2){
-        for(int j = 2; j < i; j+=2){
-            dp[i] += dp[j] * dp[i-j];
+    
+    for(int i = 4; i <= n; i+=2){
+        dp[i] = dp[i-2] * dp[2];
+        for(int j = 4; j < i; j+=2){
+            dp[i] += dp[i-j] * 2;
         }
+        dp[i] += 2;
     }
 
     cout << dp[n] << endl;
